053b0fb3f057b407ca9349945f6a0810793cd0f1
lllamas-debug <lllamas@itracasa.es>
2024-06-10 11:49:11 +0200

    Add files via upload

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'react', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "no-console": "warn",
+    "curly": "error",
+  },
+}
